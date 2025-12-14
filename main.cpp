41cce77b0a13579e097670e553bc9d674d190ddd
Amara <amara.whitson-akpamgbo01@student.csulb.edu>
2025-12-14 12:20:41 -0800

    set a default window size

--- main.cpp
+++ main.cpp
@@ -235,7 +235,9 @@ static void activate(GtkApplication *app, gpointer user_data)
     // main window
     GtkWidget *window = gtk_application_window_new(app);
     gtk_window_set_title(GTK_WINDOW(window), "Grade Calculator Practice");
-    gtk_window_set_default_size(GTK_WINDOW(window), 800, 500);
+gtk_window_set_default_size(GTK_WINDOW(window), 800, 500);
+gtk_window_set_resizable(GTK_WINDOW(window), FALSE);
+
 
     // basic styling (kept simple on purpose)
     GtkCssProvider *provider = gtk_css_provider_new();
