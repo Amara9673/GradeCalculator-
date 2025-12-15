9793000e41349289ab57e950aa90a3b341f75afa
Amarachukwu Whitson-Akpamgbo <amarawhitson@gmail.com>
2025-12-14 20:14:39 -0800

    added addxStudents to myclass.h due to compile error

--- myClass.h
+++ myClass.h
@@ -1,84 +1,75 @@
 #ifndef CLASS
 #define CLASS
 
-
 #include <string>
 #include <vector>
 #include "grade.h"
 #include "student.h"
 
 class myClass{
-    //public interface
-    public:
-        //constructor
-        myClass();   //default constructor
-        myClass(std::vector<int> weight, std::vector<int> items, std::vector<int> maxScore, std::vector<int> drops, std::vector<student> students);    //overloaded constructor
-        //big three constructors
-        myClass(const myClass& other);                  //creates a copy of an existing object
-        myClass& operator=(const myClass& other);       //assigns data from one object to another
-        //~student();                                     //destructor
+public:
+    // constructors
+    myClass();   
+    myClass(std::vector<int> weight,
+            std::vector<int> items,
+            std::vector<int> maxScore,
+            std::vector<int> drops,
+            std::vector<student> students);
 
-        //member functions
-        //accessors
-        std::vector<int> getWeight() const;          //returns data
-        std::vector<int> getItems() const;
-        std::vector<int> getMaxScore() const;
-        std::vector<int> getDrops() const;
-        std::vector<student> getStudents() const;
+    // big three
+    myClass(const myClass& other);
+    myClass& operator=(const myClass& other);
+    //~myClass();
 
-        //mutator
-        void setWeight(const std::vector<int>& nWeight);         //assigns data
-        void setItems(const std::vector<int>& nItems);
-        void setMaxScore(const std::vector<int>& nMaxScore);
-        void setDrops(const std::vector<int>& nDrops);
-        void setStudents(const std::vector<student>& nStudents);
-    
-        //functions
-        student createBaseStudent();                //creates student with max scores
-        void printBaseStudent();
-        student getStudent(const int& index);       //returns specific student object
-        void setStudent(const student& nStudent, const int& index);
-        void addStudent();                          //appends student with randomly generated values
-        void removeStudent(const int& index);       //removes student at an index
-        int getStudentsLength();                    //returns number of students
-        std::string getLetter(const int& index);    //returns letter grade of student at an index
-        double getNumLetter(const std::string& letter);     //returns number of students receiving a specific letter grade
-        std::vector<double> getPercentLetter();     //returns vector of percentage of students with grades, w/index[0]->A  &  index[4]->F
-        double getLabScore(const int& index);       //returns lab score of a specific student
-        double getQuizScore(const int& index);      //returns quiz score of a specific student
-        double getMidtermScore(const int& index);   //returns midterm score of a specific student
-        double getProjectScore(const int& index);   //returns project score of a specific student
-        double getFinalScore(const int& index);     //returns final score of a specific student
-        double getClassLabScore();                  //returns lab score of the class
-        double getClassQuizScore();                 //returns quiz score of the class
-        double getClassMidtermScore();              //returns midterm score of the class
-        double getClassProjectScore();              //returns project score of the class
-        double getClassFinalScore();                //returns final score of the class
-        double getTotalScore(const int& index);     //returns total score of a specific student
-        void sortID();                              //sorts students in ascending ID order
-        void sortLetter();                          //sorts students in descending grade letter order
-        void sortGrade();                           //sorts students in descending grade percentage order
-        void printStudents();                       //prints students' data
-        void printClass();                          //prints class' data
+    // accessors
+    std::vector<int> getWeight() const;
+    std::vector<int> getItems() const;
+    std::vector<int> getMaxScore() const;
+    std::vector<int> getDrops() const;
+    std::vector<student> getStudents() const;
 
+    // mutators
+    void setWeight(const std::vector<int>& nWeight);
+    void setItems(const std::vector<int>& nItems);
+    void setMaxScore(const std::vector<int>& nMaxScore);
+    void setDrops(const std::vector<int>& nDrops);
+    void setStudents(const std::vector<student>& nStudents);
 
+    // core functions
+    student createBaseStudent();
+    void printBaseStudent();
+    student getStudent(const int& index);
+    void setStudent(const student& nStudent, const int& index);
+    void addStudent();
+    void addxStudents(const int& num);   // 
+    void removeStudent(const int& index);
+    int getStudentsLength();
+    std::string getLetter(const int& index);
+    double getNumLetter(const std::string& letter);
+    std::vector<double> getPercentLetter();
+    double getLabScore(const int& index);
+    double getQuizScore(const int& index);
+    double getMidtermScore(const int& index);
+    double getProjectScore(const int& index);
+    double getFinalScore(const int& index);
+    double getClassLabScore();
+    double getClassQuizScore();
+    double getClassMidtermScore();
+    double getClassProjectScore();
+    double getClassFinalScore();
+    double getTotalScore(const int& index);
+    void sortID();
+    void sortLetter();
+    void sortGrade();
+    void printStudents();
+    void printClass();
 
-        //add overloading to use desired operators on class
-        //friend allows access to private data
-        //friend std::ostream& operator<<(std::ostream& out, const myClass& class);
-        //friend std::istream& operator>>(std::istream& in, student& student);
-
-        
-
-    
-    //private data fields
-    private:
-        std::vector<int> weight;
-        std::vector<int> items;
-        std::vector<int> maxScore;
-        std::vector<int> drops;
-        std::vector<student> students;
-
+private:
+    std::vector<int> weight;
+    std::vector<int> items;
+    std::vector<int> maxScore;
+    std::vector<int> drops;
+    std::vector<student> students;
 };
 
 #endif
